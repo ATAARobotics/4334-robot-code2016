c4bf6a7710da6c9028e6940738e95c86b69a4d15
aidenbenner <aiden.benner@gmail.com>
2016-01-10 23:08:14 -0700

    initial attempt, added generic PID class

---
+++ src/Utils.java
@@ -0,0 +1,18 @@
+
+
+public class Utils {
+	
+	
+	public static double deadzone(double input, double min){
+		return Math.abs(input) < Math.abs(min) ?  0 : input;
+	}
+	
+	
+	//returns input clamped between -max and max
+	public static double clamp(double input, double max){
+		if(Math.abs(input) > max){
+			return max * input/Math.abs(input);
+		}
+		return input;
+	}
+}
