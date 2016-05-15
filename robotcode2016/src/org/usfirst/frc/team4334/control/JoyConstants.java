c4a059710cfcbe3151076b6095e032680acf3273
aidenbenner <aiden.benner@gmail.com>
2016-05-15 14:27:56 -0600

    code cleanup

--- robotcode2016/src/org/usfirst/frc/team4334/control/JoyConstants.java
+++
@@ -1,6 +0,0 @@
-package org.usfirst.frc.team4334.control;
-
-public class JoyConstants {
-	public static final double ARM_DEADZONE = 0.17;
-	
-}
--- robotcode2016/src/org/usfirst/frc/team4334/control/Loopable.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.team4334.control;
-
-public interface Loopable {
-	public void update();
-}
